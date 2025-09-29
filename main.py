046de31c9d4fd1506c7b94ca56ee5534d344563c
kasohrab <kasra.sohrab@gmail.com>
2025-09-29 01:59:21 -0400

    dependencies

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import torch
+from model import TheWorld
+
 def main():
-    print("Hello from project!")
+    model = TheWorld("google/gemma-3-1b-it")
+    model.forward(torch.randn(1, 3, 224, 224), torch.randint(0, 100, (1, 10)), torch.randint(0, 1, (1, 10)))
 
 
 if __name__ == "__main__":
